19fcafa4af84bd46684f56cfe57ccf9bf0b38543
Ionel Stefan <45713280+smiromania@users.noreply.github.com>
2026-01-12 07:24:41 +0200

    Add fileHelpers.js with convertToBase64 function

---
+++ src/utils/fileHelpers.js
@@ -0,0 +1,13 @@
+// utils/fileHelpers.js
+export const convertToBase64 = (file) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+};
